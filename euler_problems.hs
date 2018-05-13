1a305befcf8aefd67c11f0f9c2796924259d0a32
Ed Allonby <edward.allonby@gmail.com>
2018-05-13 17:58:18 +0100

    add problem 2

--- euler_problems.hs
+++ euler_problems.hs
@@ -1,4 +1,5 @@
-
 isThreeOrFive = \x -> (x `mod` 3 == 0) || (x `mod` 5 == 0)
-
 problem_1 = sum (filter isThreeOrFive [1..999])
+
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+problem_2 = sum (takeWhile (<4000000) (filter even fibs))
