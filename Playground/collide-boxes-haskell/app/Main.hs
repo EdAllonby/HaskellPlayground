dbf2a76d854dcf0d357acd8e42db7d1c3fdc8749
Ed Allonby <edward.allonby@gmail.com>
2019-08-29 21:38:21 +0100

    Update collide boxes stuff

--- Playground/collide-boxes-haskell/app/Main.hs
+++ Playground/collide-boxes-haskell/app/Main.hs
@@ -4,6 +4,7 @@
 module Main where
 
 import           Control.Lens
+
 import           Graphics.Gloss
 import           Graphics.Gloss.Data.Color
 
@@ -12,12 +13,12 @@ window = InWindow "Sandbox" (1500, 600) (100, 100)
 fps = 60
 
 data Box =
-  Box { _xPos :: Float, _mass :: Float, _size :: Float, _velocity :: Float }
-  deriving (Eq, Show)
+    Box { _xPos :: Float, _mass :: Float, _size :: Float, _velocity :: Float }
+    deriving ( Eq, Show )
 
 data Sandbox =
-  Sandbox { _leftBox :: Box, _rightBox :: Box, _collisions :: Int }
-  deriving (Eq, Show)
+    Sandbox { _leftBox :: Box, _rightBox :: Box, _collisions :: Int }
+    deriving ( Eq, Show )
 
 makeLenses ''Box
 
@@ -33,17 +34,16 @@ main :: IO ()
 main = play window backgroundColor fps initialSandbox draw (const id) frame
 
 frame :: Float -> Sandbox -> Sandbox
-frame delta sandbox =
-  if collided (sandbox ^. leftBox) (sandbox ^. rightBox)
-  then (updateCount . moveLeft . moveRight . updateVelocitiesAfterBounce)
-    sandbox
-  else (moveLeft . moveRight) sandbox
+frame delta sandbox = if collided (sandbox ^. leftBox) (sandbox ^. rightBox)
+                      then (updateCount . moveLeft . moveRight
+                            . updateVelocitiesAfterBounce) sandbox
+                      else (moveLeft . moveRight) sandbox
   where
-    moveLeft = updateBoxPosition delta leftBox
+    moveLeft    = updateBoxPosition delta leftBox
 
-    moveRight = updateBoxPosition delta rightBox
+    moveRight   = updateBoxPosition delta rightBox
 
-    bounceLeft = bounce leftBox rightBox
+    bounceLeft  = bounce leftBox rightBox
 
     bounceRight = bounce rightBox leftBox
 
@@ -52,47 +52,45 @@ updateCount sandbox = sandbox & collisions %~ (+ 1)
 
 updateBoxPosition :: Float -> Lens' Sandbox Box -> Sandbox -> Sandbox
 updateBoxPosition delta boxLens sandbox =
-  if hitWall $ sandbox ^. boxLens
-  then updateCount
-    $ getNextPosition delta boxLens
-    $ over boxLens reverseBox sandbox
-  else getNextPosition delta boxLens sandbox
+    if hitWall $ sandbox ^. boxLens
+    then updateCount $ getNextPosition delta boxLens $
+        over boxLens reverseBox sandbox
+    else getNextPosition delta boxLens sandbox
 
 getNextPosition :: Float -> Lens' Sandbox Box -> Sandbox -> Sandbox
-getNextPosition delta boxLens sandbox = over
-  (boxLens . xPos)
-  (+ (delta * fromIntegral fps * (sandbox ^. boxLens . velocity)))
-  sandbox
+getNextPosition delta boxLens sandbox =
+    over (boxLens . xPos)
+         (+ (delta * fromIntegral fps * (sandbox ^. boxLens . velocity)))
+         sandbox
 
 updateVelocitiesAfterBounce :: Sandbox -> Sandbox
-updateVelocitiesAfterBounce sandbox = sandbox
-  & (leftBox . velocity) .~ newLeftVelocity
-  & (rightBox . velocity) .~ newRightVelocity
+updateVelocitiesAfterBounce sandbox = sandbox & (leftBox . velocity)
+    .~ newLeftVelocity & (rightBox . velocity) .~ newRightVelocity
   where
-    newLeftVelocity = bounce leftBox rightBox sandbox
+    newLeftVelocity  = bounce leftBox rightBox sandbox
 
     newRightVelocity = bounce rightBox leftBox sandbox
 
 bounce :: Lens' Sandbox Box -> Lens' Sandbox Box -> Sandbox -> Float
 bounce targetBox otherBox sandbox = newV
   where
-    targetMass = sandbox ^. (targetBox . mass)
+    targetMass     = sandbox ^. (targetBox . mass)
 
-    otherMass = sandbox ^. (otherBox . mass)
+    otherMass      = sandbox ^. (otherBox . mass)
 
     targetVelocity = sandbox ^. (targetBox . velocity)
 
-    otherVelocity = sandbox ^. (otherBox . velocity)
+    otherVelocity  = sandbox ^. (otherBox . velocity)
 
-    sumM = targetMass + otherMass
+    sumM           = targetMass + otherMass
 
-    newV = (targetMass - otherMass) / sumM * targetVelocity
-      + ((2 * otherMass / sumM) * otherVelocity)
+    newV           = (targetMass - otherMass) / sumM * targetVelocity
+        + ((2 * otherMass / sumM) * otherVelocity)
 
 collided :: Box -> Box -> Bool
 collided left right = leftEdge >= rightEdge
   where
-    leftEdge = (left ^. xPos) + (left ^. size / 2)
+    leftEdge  = (left ^. xPos) + (left ^. size / 2)
 
     rightEdge = (right ^. xPos) - (right ^. size / 2)
 
@@ -103,14 +101,20 @@ reverseBox :: Box -> Box
 reverseBox = over velocity negate
 
 draw :: Sandbox -> Picture
-draw sandbox = pictures
-  [ color black (line [(0, -300), (-0, 300)])
-  , translate (sandbox ^. rightBox . xPos) (sandbox ^. rightBox . size / 2)
-      $ color red
-      $ rectangleSolid
-        (sandbox ^. rightBox . size)
-        (sandbox ^. rightBox . size)
-  , translate (sandbox ^. leftBox . xPos) (sandbox ^. leftBox . size / 2)
-      $ color blue
-      $ rectangleSolid (sandbox ^. leftBox . size) (sandbox ^. leftBox . size)
-  , text $ show (sandbox ^. collisions)]
+draw sandbox =
+    pictures [ color black (line [ (0, -300), (-0, 300) ])
+             , translate (sandbox ^. rightBox . xPos)
+                         (sandbox ^. rightBox . size / 2) $ color red $
+                   rectangleSolid (sandbox ^. rightBox . size)
+                                  (sandbox ^. rightBox . size)
+             , translate (sandbox ^. leftBox . xPos)
+                         (sandbox ^. leftBox . size / 2) $ color blue $
+                   rectangleSolid (sandbox ^. leftBox . size)
+                                  (sandbox ^. leftBox . size)
+             , text $ show (sandbox ^. collisions)
+             ]
+
+data Foo = Foooo | Bar
+
+test1234 Foooo = 1
+test1234 Bar   = 2
--- Playground/collide-boxes-haskell/src/Lib.hs
+++ Playground/collide-boxes-haskell/src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( someFunc ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+test :: String
+test = ""
+
+data Foo a = _Foo | Bar a
+
+usedFor :: Num p => Foo p -> p
+usedFor Foo     = 1
+usedFor (Bar a) = a
